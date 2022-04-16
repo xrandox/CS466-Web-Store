787ed17fd6b423997d1c4bb0bd3ddc2a4cc5203c
ryanipark <ryanpark@usa.com>
2022-04-16 11:08:28 -0500

    Update dbInsert.sql
    Added 20 products into the script, did not test yet

--- dbInsert.sql
+++ dbInsert.sql
@@ -1,5 +1,69 @@
 /*File for database INSERT scripts*/
 
-INSERT INTO ___ ( )
-VALUES
-    ( );
+/*
+ *
+ * Insert 20 products into the database
+ *
+ */ 
+
+INSERT INTO products
+	values('Batman Headphones',
+           'Echo location headphones, allows for up to 24 hours of stream time',
+           25,
+           125
+           );
+
+INSERT INTO products
+	values('Fuzzy dice',
+           'Black dice with white dots, wrap them around your rear view mirror!',
+           100,
+           8
+           );
+INSERT INTO products
+	values('Wireless mouse',
+           'Uses 2 AA batteries, comes with a FREE mouse pad.',
+           30,
+           18.50
+           );
+INSERT INTO products
+	values('Lockpicking set',
+           'Pack contains 8 different tools and a get started guide!',
+           75,
+           8
+           );
+INSERT INTO products
+	values('OBD2 Scanner',
+           'Reads engine codes, and diagnoses problems.',
+           5,
+           125.25
+           );
+INSERT INTO products
+	values('Camping tent',
+           'This tent can fit 4 people comfortably!, made with only the best material!',
+           15,
+           99.99
+           );
+INSERT INTO products
+	values('American flag',
+           'Show your colors with this 3ft x 5ft incredibly durable flag!',
+           35,
+           14.99
+           );
+INSERT INTO products
+	values('Neck pillow',
+           'Peak sleeping performance on the go, made out of 100% cotton.',
+           55,
+           8
+           );
+INSERT INTO products
+	values('Baby stroller',
+           'Seats one child in luxury with two cupholders and tons of storage space.',
+           10,
+           49.99
+           );
+INSERT INTO products
+	values('800cc Lawn Mower',
+           'Tired of wasting your time cutting grass? This high performance lawn mower garuntees a better cut in 50% less time then a normal mower.',
+           10,
+           249.99
+           );
