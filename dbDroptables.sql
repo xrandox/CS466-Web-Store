eeea62510afbb489ecfca0f33a88b2c4913907a9
ryanipark <ryanpark@usa.com>
2022-04-17 15:02:06 -0500

    Create dbDroptables.sql
    script to drop tables

---
+++ dbDroptables.sql
@@ -0,0 +1,11 @@
+/*
+ * Script to drop all the tables created from dbCreate.sql
+ */
+
+DROP TABLE shoppingCart;
+DROP TABLE orderProducts;
+DROP TABLE orders;
+DROP TABLE products;
+DROP TABLE users;
+DROP TABLE orderInfo;
+SHOW TABLES;
