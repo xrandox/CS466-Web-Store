076eafd109bb52e526dc5da43e4f62cee9c99e61
null <xrandox17@yahoo.com>
2022-04-20 00:30:19 -0500

    moved userUtil to util directory

--- util/userUtil.php
+++ util/userUtil.php
@@ -1,11 +1,12 @@
+<!--user utilities-->
 <?php
-    require_once("../util/creds.php");
-    require_once("../util/sessionStart.php");
-    require_once("../util/sqlFunc.php");
+    require_once("./creds.php");
+    require_once("./sessionStart.php");
+    require_once("./sqlFunc.php");
 
     //checks if user $uid has $level and returns a boolean, true if they do, false if not
     //$level 0 = normal user, 1 = employee, 2 = owner
-    function privCheck($uid, $level)
+    function privCheck($pdo, $uid, $level)
     {
         $userInfo = fetch($pdo, "SELECT * FROM users WHERE userID=?", [$uid]);
 
@@ -16,10 +17,10 @@
             else return false;
         }
 
-        //employee check
+        //atleast an employee (also counts owner)
         if ($level = 1)
         {
-            if ($userInfo['isEmployee'] == 1) return true;
+            if ($userInfo['isOwner'] == 1 || $userInfo['isEmployee'] == 1) return true;
             else return false;
         }
 
@@ -44,31 +45,33 @@
         }
     }
 
+    //lists all orders given an array of orders
     function listOrders($orders)
     {
-        if ($orders = []) 
+        //if it's empty, there's nothing to list
+        if ($orders == []) 
         {
-            echo "No orders to show";
+            echo "No orders to show<br>";
             return;
         }
 
-        $orderNum = 1;
+        //loop through list displaying each order
         echo "Click on the order for a more detailed view<br>";
         foreach ($orders as $order)
         {
+            echo "<br>";
             //assign vars for convenience
             $orderID = $order['orderID'];
             $total = $order['total'];
             $shippingNumber = $order['shippingNumber'];
-            $status = statusConverter($order['orderStatus']);
+            $status = statusConverter($order['orderStatus']); //convert status to readable string
 
-            echo "<a href='./orderDetails.php?orderID=$orderID'>Order Number $orderNum:</a><br>
-            Order ID: #$orderID<br>
-            Total Paid: $total<br>
-            Order Status: $status<br>";
+            //echo a link to order details page and general order info
+            echo "<a href='./orderDetails.php?orderID=$orderID'>Order ID: #$orderID</a><br>
+            Total Paid: $$total<br>
+            Status: $status<br>";
             if ($shippingNumber != "") echo "Shipping Number: $shippingNumber<br>";
             
-            $orderNum += 1;
         }
         return;
     }
