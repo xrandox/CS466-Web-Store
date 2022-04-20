92fa41ccf7f671a0d8a6f927a9b6f77e6bfac654
null <xrandox17@yahoo.com>
2022-04-19 22:18:29 -0500

    created base user order page

---
+++ user/userOrders.php
@@ -0,0 +1,29 @@
+<?php
+    require_once("../util/creds.php");
+    require_once("../util/sessionStart.php");
+    require_once("../util/sqlFunc.php");
+
+    $uid = $_SESSION['uid'];
+
+    $userOrders = fetchAll($pdo, "SELECT * FROM orders WHERE userID=? AND orderStatus>0", [$uid]);
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link rel="stylesheet" href="style.php">
+        <title>Web Store - User Orders</title>
+    </head>
+
+    <body>
+        <h1>Orders:<h1>
+        <?php
+            listOrders($userOrders);
+        ?>
+
+        <a href='./userProfile.php'><button type='button'>Return to User Profile</button><a/><br>";
+    </body>
+
+</html>
