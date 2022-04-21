73f8756d59e8d514fd196cc8119982f97cf9bcf6
Caleb P <Z1930375@students.niu.edu>
2022-04-20 20:18:22 -0500

    Implemented a "Add to Shopping Cart" button

--- productDetails.php
+++ productDetails.php
@@ -8,25 +8,71 @@
 ?>
 <html>
 <body>
-<style>
-    .aProduct{
-        border-style: dotted solid;
-        border-width: 1px;
-        background-color: grey;
-        text-align: center;
-    }
-</style>   
-<h2>Product Details:</h2>
+    <style>
+        .pDetails
+        {
+            border-style: solid;
+            border-width: 1px;
+            padding: 5px 25px 5px 25px;
+            text-align: center;
+        }
+        .pImage
+        {
+            padding: 25px 0px 25px 0px;
+            border-width: 1px;
+            text-align: center;
+        }
+        .pName
+        {
+            text-align: center;
+        }
+    </style>  
     <?php
     foreach($rows as $product)
     {
     ?>
-        <div class="aProduct"> 
-        <h2><?php echo $product['prodName']?></h2>
-        <p><?php echo "Description: " . $product['descr']?></p>
-        <p><?php echo $product['qtyAvailable'] . " in stock"?></p>
+        <div class="pName"> 
+            <h2><?php echo $product['prodName']?></h2>
         </div>
+
+        <div class="pImage">
+            <img src=<?php echo $product['imageLink']?> />
+        </div>
+
+        <div class="pDetails">
+            <p><?php echo "Description: " . $product['descr']?></p>
+            <?php echo $product['qtyAvailable'] . " in stock"?></p>
+
+            <form method="POST">
+                <label for="qtyWanted">Quanty to order: </label>
+                <input type="number" name="qtyWanted" min="0" max="<?php echo $product['qtyAvailable']?>" value='0'/><br><br>
+                <input type="submit" value="Add to Shopping Cart"/> 
+            </form>
+
+        </div>
+    <!--
+        
+    -->
+
         <?php
+        $uid = $_SESSION['uid'];
+        $pid = $_GET['prodID'];
+
+        if ($_POST["qtyWanted"] > 0)
+        {
+            $rs = $pdo->prepare("INSERT INTO shoppingcart (userID, prodID, qty) VALUES ( :pu, :pid, :pw );");
+            $success = $rs->execute(array(':pu' => $uid,':pid' => $pid, ':pw' => $_POST["qtyWanted"]));
+    
+            if($success)
+            {
+                echo 'Product added to the shopping cart.';
+            }
+            else
+            {
+                echo 'Error';
+            }
+        }
+        
     }
     ?>  
 </body>
