0000407150a55145f309faacfb5ebebe80a636bd
Caleb P <Z1930375@students.niu.edu>
2022-04-21 20:48:32 -0500

    Shows order number and status of orders

--- order/ordersOutstanding.php
+++ order/ordersOutstanding.php
@@ -1,14 +1,72 @@
 <?php
-    require_once("./util/creds.php");
-    require_once("./util/sessionStart.php");
+    require_once("../util/creds.php");
+    require_once("../util/sessionStart.php");
 
-    $rs = $pdo->prepare("SELECT * FROM products WHERE prodID = ?;");
-    $rs->execute(array($_GET["prodID"]));
-    $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
+    echo $_SESSION['uid'];
 ?>
 <html>
 <body>
-   
+    <style>
+        .order
+        {
+            border-style: solid;
+            border-width: 1px;
+            padding: 5px 5px 5px 5px;
+        }
+    </style>
+    <?php
+        try
+        {
+            $rs = $pdo->query("SELECT * FROM orders;");
+            $orders = $rs->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch(PDOexception $e) 
+        {
+            echo "Connection was not established to database, with reason: " . $e->getMessage();
+        }
+
+        foreach($orders as $order)
+        {
+    ?>
+
+        <div class="order">
+            <p><?php echo "Order Number: " . $order['orderID'];?></p>
+           <!-- <p><?php echo "User ID: " . $order['userID']?></p>
+            <p><?php echo "Shipping ID: " . $order['shippingID']; ?></p>
+            <p><?php echo "Billing ID: " . $order['billingID']; ?></p>
+            <p><?php echo "Order Total: $" . $order['total']; ?></p>
+            <p><?php echo "Shipping Number: " . $order['shippingNumber']; ?></p> -->
+
+
+            <p><?php
+                    $status = $order['orderStatus'];
+                    $statusString = "";
+
+                    switch ($status)
+                    {
+                        case 0:
+                            $statusString = "Checkout";
+                            break;
+                        case 1:
+                            $statusString = "Order Success";
+                            break;
+                        case 2:
+                            $statusString = "Processing";
+                            break;
+                        case 3:
+                            $statusString = "Shipped";
+                            break;
+                    }
+
+                    echo "Order Status: " . $statusString;
+               ?></p>
+           <!-- <p><?php echo "Notes: " . $order['notes']?></p>-->
+           <a href="./ordersFulfillment.php<?php echo "?orderID=" . $order['orderID']?>"><button type="button">View Order</button></a><br>
+        </div>
+
+    <?php
+        }
+    ?>   
 
 </body>
 </html>
