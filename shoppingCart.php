b57e66853ba6428a9afd98e1fd23eeb58e1f20a7
null <xrandox17@yahoo.com>
2022-04-29 08:59:53 -0500

    Update shoppingCart.php

--- shoppingCart.php
+++ shoppingCart.php
@@ -7,95 +7,110 @@
     require_once("./util/checkoutUtil.php");
     require_once("./util/sqlFunc.php");
     require_once("./util/userUtil.php");
+
+//Moved this because we want it to execute first thing when the page reloads, and it's better to think of this happening outside of the main loop, hence why we must pass all info via POST/GET
+
+if(isset($_POST["update"])) 
+{
+    //using hidden PID to target the correct product
+    $pidToUpdate = $_POST["prodID"];
+
+    //get the difference between what used to be in the cart
+    $diff = $_POST["oldQty"] - $_POST["qtyChange"];
+
+    //if something changed, update
+    if ($diff != 0) 
+    {
+        //using execute from sqlFunc
+        $stmt = execute($pdo, "UPDATE shoppingcart SET qty=? WHERE prodID=?", [$_POST["qtyChange"], $pidToUpdate]);
+        $stmt2 =  execute($pdo, "UPDATE products SET qtyAvailable=qtyAvailable+? WHERE prodID=?", [$diff, $pidToUpdate]);
+
+        //if either fails
+        if(!$stmt || !$stmt2)
+        {
+            //failed to update smth
+        }
+    }
+}
+
 ?>
 
+<!--I reformatted things to make it easier for me to see how your code was structured, sorry! Feel free to change back if you prefer how you had it-->
 <!DOCTYPE html>
-<head>
-    <link rel="stylesheet" href="style.php">
-    <title>Shopping Cart</title>
-</head>
+<html>
+    <head>
+        <title>Shopping Cart</title>
+    </head>
 
-<body>
-    <h1>Shopping Cart</h1>
+    <body>
+        <?php require_once("./style/nav.php"); navBar(); //adds navbar to top of page ?> 
+        <h1 style='margin-top:65px;font-family:Consolas;text-align:center;'>Shopping Cart</h1> <!--Added styling here to fit with navbar-->
 
-    <?php
-        $uid = $_SESSION['uid'];
-
-        //get Subtotal for shopping cart
-        getCartTotal($pdo, $uid);
-        $total = $_SESSION['cartTotal'];
-
-        $products = fetchAll($pdo, "SELECT * FROM shoppingCart WHERE userID = ? AND qty > 0", [$uid]);
-
-        if($products == [])
-        {
-            echo "Shopping Cart is Empty<br>
-            <a href='./productList.php'><button type='button'>Back to Shopping</button></a>";
-            return;
-        }
-    ?>
-
-
-<!--Shopping table-->
-<table border=1 cellspacing=1>
-    <thead>
-        <tr>
-            <th><b>Product</b></th>
-            <th><b>Price</b></th>
-            <th><b>Quantity</b></th>
-        </tr>
-    </thead>
-
-    <tbody>
         <?php
-            foreach($products as $product)
+            $uid = $_SESSION['uid'];
+
+            //get Subtotal for shopping cart
+            getCartTotal($pdo, $uid);
+            $total = $_SESSION['cartTotal'];
+
+            $products = fetchAll($pdo, "SELECT * FROM shoppingCart WHERE userID = ? AND qty > 0", [$uid]);
+
+            if($products == [])
             {
-                //create the product variables
-                $qty = $product['qty'];
-                $prodInfo = fetch($pdo, "SELECT * FROM products WHERE prodID = ?", [$product['prodID']]);
-                $prodName = $prodInfo['prodName'];
-                $price = (float) $qty * $prodInfo['price'];
-                
-                
+                echo "Shopping Cart is Empty<br>
+                <a href='./productList.php'><button type='button'>Back to Shopping</button></a>";
+                return;
+            }
         ?>
-<!-- Form the Shopping Cart Table-->
+
+
+        <!--Shopping table-->
+        <table border=1 cellspacing=1>
+            <thead>
+                <tr>
+                    <th><b>Product</b></th>
+                    <th><b>Price</b></th>
+                    <th><b>Quantity</b></th>
+                </tr>
+            </thead>
+
+            <tbody> 
+                <?php
+                foreach($products as $product)
+                {
+                    //create the product variables
+                    $qty = $product['qty'];
+                    $prodInfo = fetch($pdo, "SELECT * FROM products WHERE prodID = ?", [$product['prodID']]);
+                    $prodName = $prodInfo['prodName'];
+                    $price = (float) $qty * $prodInfo['price'];
+                    $pid = $product['prodID'];
+                                
+                ?>
+
+                <!-- Form the Shopping Cart Table-->
                 <tr>
                 <td><?php echo "$prodName"; ?></td>
                 <td><?php echo "$$price";?></td>
                 <td>
+                    <!--Condensed forms to 1, set min to 0, added hidden value to pass pid and the original qty-->
                     <form method='POST'>
-                        <input type="number" name="qtyChange" min=<?php echo $qty?> max= <?php echo $prodInfo['qtyAvailable']?> value = "<?php echo $qty?>"/>
-                     </form>
+                        <input type="number" name="qtyChange" min=0 max=<?php echo $prodInfo['qtyAvailable']?> value="<?php echo $qty?>"/> 
                 </td>
                 <td>
-                    <form method='POST'>
-                        <a href='shoppingCart.php'><input type="submit" name="removeit" value="Remove"/>
+                        <input type="hidden" name="oldQty" value="<?php echo $qty?>"/>
+                        <input type="hidden" name="prodID" value="<?php echo $pid?>"/>
+                        <input type="submit" name="update" value="Update"/>
+                    </form>
                 </tr>
-        <?php
-                $pid = $product['prodID'];
-                
-                if(isset($_POST["removeit"]))
-                {
-                    $rp = $pdo->prepare("DELETE FROM shoppingCart WHERE prodID = :pd;");
-                    $rs = $rp ->execute(array(':pd' => $pid));
-                    
-                }
-                
-                if(isset($_POST["qtyChange"]))
-                {
-                    
-                }
-            }
-        ?>    
-    
-    </tbody>
-</table>
 
-<p>SubTotal: $<?php echo $total?></p>
-<br>
-<a href="shoppingCart.php"><button type='button'>Update Cart</button></a>
-<a href='./productList.php'><button type='button'>Back to Shopping</button></a>
-</body>
+                <?php 
+                }
+                ?>            
+            </tbody>
+        </table>
+
+        <p>SubTotal: $<?php echo $total?></p>
+        <br>
+        <a href='./productList.php'><button type='button'>Back to Shopping</button></a>
+    </body>
 </html>
-
-
